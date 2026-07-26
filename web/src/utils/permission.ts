36f0d7ce7dc125959d1fe6dfc1383b0f02617254
Wang <w505503784@163.com>
2026-07-26 09:36:11 +0800

    整合了3天做出来的1100行架构说明MD，现在从零开始

--- web/src/utils/permission.ts
+++
@@ -1,6 +0,0 @@
-import { usePermissionStore } from '@/stores/permission'
-
-export function hasPermission(key: string): boolean {
-  const permissionStore = usePermissionStore()
-  return permissionStore.hasPermission(key)
-}
